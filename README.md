aa924541780883aa6ac7a8a193dc730591085caf
foxx3r <coluna123@protonmail.ch>
2020-05-13 00:27:08 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # definite_clause_grammars
 A script based on the concept DCG (Definite Clause Grammars).
+
+To run that script, run on swipl:
+
+`?- [definite_clause_grammars].`
+
+And then, you will can test all those functions spicified in the script. An example:
+
+```
+?- possessive_phrase(X, [and, it, is, so, beautiful]).
+X = [this, is, my, tree, and, it, is, so, beautiful] ;
+X = [this, is, my, car, and, it, is, so, beautiful] ;
+X = [this, is, my, cellphone, and, it, is, so, beautiful] ;
+X = [this, is, my, tv, and, it, is, so, beautiful] ;
+X = [this, is, your, tree, and, it, is, so, beautiful] ;
+X = [this, is, your, car, and, it, is, so, beautiful] ;
+X = [this, is, your, cellphone, and, it, is, so, beautiful] ;
+X = [this, is, your, tv, and, it, is, so, beautiful] ;
+X = [this, is, his, tree, and, it, is, so, beautiful] ;
+X = [this, is, his, car, and, it, is, so, beautiful] ;
+X = [this, is, his, cellphone, and, it, is, so, beautiful] ;
+X = [this, is, his, tv, and, it, is, so, beautiful] ;
+X = [this, is, her, tree, and, it, is, so, beautiful] ;
+X = [this, is, her, car, and, it, is, so, beautiful] ;
+X = [this, is, her, cellphone, and, it, is, so, beautiful] ;
+X = [this, is, her, tv, and, it, is, so, beautiful] .
+
+?- future_perfect_progressive_phrase(X, []).
+X = [i, will, have, been, playing] ;
+X = [i, will, have, been, talking] ;
+X = [i, will, have, been, riding] ;
+X = [i, will, have, been, waking] ;
+X = [i, will, have, been, sitting] ;
+X = [i, will, have, been, yawning] ;
+X = [you, will, have, been, playing] ;
+X = [you, will, have, been, talking] ;
+X = [you, will, have, been, riding] ;
+X = [you, will, have, been, waking] .
+
+?- superlative_phrase(X, [of, the, world]).
+X = [i, am, the, worst, of, the, world] ;
+X = [i, am, the, prettiest, of, the, world] ;
+X = [i, am, the, strongest, of, the, world] ;
+X = [i, am, the, coolest, of, the, world] ;
+X = [i, am, the, bestest, of, the, world] ;
+X = [i, am, a, worst, of, the, world] ;
+X = [i, am, a, prettiest, of, the, world] ;
+X = [i, am, a, strongest, of, the, world] ;
+X = [i, am, a, coolest, of, the, world] ;
+X = [i, am, a, bestest, of, the, world] ;
+X = [you, are, the, worst, of, the, world] ;
+X = [you, are, the, prettiest, of, the, world] ;
+X = [you, are, the, strongest, of, the, world] ;
+X = [you, are, the, coolest, of, the, world] ;
+X = [you, are, the, bestest, of, the, world] ;
+X = [you, are, a, worst, of, the, world] ;
+X = [you, are, a, prettiest, of, the, world] ;
+X = [you, are, a, strongest, of, the, world] ;
+X = [you, are, a, coolest, of, the, world] ;
+X = [you, are, a, bestest, of, the, world] ;
+X = [he, is, the, worst, of, the, world] ;
+X = [he, is, the, prettiest, of, the, world] ;
+X = [he, is, the, strongest, of, the, world] ;
+X = [he, is, the, coolest, of, the, world] ;
+X = [he, is, the, bestest, of, the, world] ;
+X = [he, is, a, worst, of, the, world] .
+```
---
+++ definite_clause_grammars.pl
@@ -0,0 +1,300 @@
+determiner --> [the].
+determiner --> [a].
+
+adverb --> [so].
+adverb --> [always].
+adverb --> [really].
+adverb --> [maybe].
+
+verb_to_be --> [i, am].
+verb_to_be --> [you, are].
+verb_to_be --> [he, is].
+verb_to_be --> [she, is].
+verb_to_be --> [it, is].
+verb_to_be --> [we, are].
+verb_to_be --> [they, are].
+
+singular_to_be --> [is].
+
+plural_to_be --> [are].
+
+adjective --> [beautiful].
+adjective --> [smart].
+adjective --> [hardworking].
+
+singular_noun --> [tree].
+singular_noun --> [car].
+singular_noun --> [cellphone].
+singular_noun --> [tv].
+
+plural_noun --> [trees].
+plural_noun --> [cars].
+plural_noun --> [arrows].
+plural_noun --> [ships].
+plural_noun --> [people].
+plural_noun --> [balls].
+
+verb --> [play].
+verb --> [ride].
+verb --> [call].
+verb --> [say].
+verb --> [want].
+verb --> [need].
+
+pronoun --> [i].
+pronoun --> [you].
+pronoun --> [he].
+pronoun --> [she].
+pronoun --> [it].
+pronoun --> [we].
+pronoun --> [they].
+
+object_pronoun --> [me].
+object_pronoun --> [you].
+object_pronoun --> [him].
+object_pronoun --> [her].
+object_pronoun --> [it].
+object_pronoun --> [us].
+object_pronoun --> [them].
+
+possessive --> [my].
+possessive --> [your].
+possessive --> [his].
+possessive --> [her].
+possessive --> [its].
+possessive --> [our].
+possessive --> [their].
+
+absolute_possessive --> [mine].
+absolute_possessive --> [yours].
+absolute_possessive --> [his].
+absolute_possessive --> [hers].
+absolute_possessive --> [its].
+absolute_possessive --> [ours].
+absolute_possessive --> [theirs].
+
+distance --> [this, is].
+distance --> [that, is].
+distance --> [these, are].
+distance --> [those, are].
+
+preposition --> [of].
+preposition --> [for].
+preposition --> [like].
+preposition --> [on].
+
+negative_verb_to_be --> [i, am, not].
+negative_verb_to_be --> [you, are, not].
+negative_verb_to_be --> [he, is, not].
+negative_verb_to_be --> [she, is, not].
+negative_verb_to_be --> [it, is, not].
+negative_verb_to_be --> [we, are, not].
+negative_verb_to_be --> [they, are, not].
+
+question --> [do, i].
+question --> [do, you].
+question --> [does, he].
+question --> [does, she].
+question --> [does, it].
+question --> [do, we].
+question --> [do, they].
+
+verb_to_be_question --> [am, i].
+verb_to_be_question --> [are, you].
+verb_to_be_question --> [is, he].
+verb_to_be_question --> [is, she].
+verb_to_be_question --> [is, it].
+verb_to_be_question --> [are, we].
+verb_to_be_question --> [are, they].
+
+interrogative_pronoun --> [what].
+interrogative_pronoun --> [where].
+interrogative_pronoun --> [which].
+interrogative_pronoun --> [whose].
+interrogative_pronoun --> [who].
+
+modal --> [will].
+modal --> [would].
+modal --> [can].
+modal --> [could].
+modal --> [shall].
+modal --> [should].
+modal --> [ought, to].
+modal --> [may].
+modal --> [might, be].
+
+gerund --> [playing].
+gerund --> [talking].
+gerund --> [riding].
+gerund --> [waking].
+gerund --> [sitting].
+gerund --> [yawning].
+
+simple_past --> [i, was].
+simple_past --> [you, were].
+simple_past --> [he, was].
+simple_past --> [she, was].
+simple_past --> [it, was].
+simple_past --> [we, were].
+simple_past --> [they, were].
+
+simple_past_negative --> [i, was, not].
+simple_past_negative --> [you, were, not].
+simple_past_negative --> [he, was, not].
+simple_past_negative --> [she, was, not].
+simple_past_negative --> [it, was, not].
+simple_past_negative --> [we, were, not].
+simple_past_negative --> [they, were, not].
+
+simple_past_question --> [was, i].
+simple_past_question --> [were, you].
+simple_past_question --> [was, he].
+simple_past_question --> [was, she].
+simple_past_question --> [was, it].
+simple_past_question --> [were, we].
+simple_past_question --> [were, they].
+
+past_verb --> [went].
+past_verb --> [saw].
+past_verb --> [worked].
+past_verb --> [mixed].
+past_verb --> [said].
+
+participle --> [taken].
+participle --> [done].
+participle --> [gone].
+participle --> [sewn].
+participle --> [sawn].
+participle --> [killed].
+participle --> [tried].
+participle --> [stolen].
+participle --> [found].
+participle --> [forgotten].
+
+conditional --> [if].
+conditional --> [whether].
+
+logical_conditional --> [and].
+logical_conditional --> [or].
+
+present_perfect --> [i, have].
+present_perfect --> [you, have].
+present_perfect --> [he, has].
+present_perfect --> [she, has].
+present_perfect --> [it, has].
+present_perfect --> [we, have].
+present_perfect --> [they, have].
+
+present_perfect_interrogative --> [have, i].
+present_perfect_interrogative --> [have, you].
+present_perfect_interrogative --> [has, he].
+present_perfect_interrogative --> [has, she].
+present_perfect_interrogative --> [has, it].
+present_perfect_interrogative --> [have, we].
+present_perfect_interrogative --> [have, they].
+
+present_continuous --> [i, have, been].
+present_continuous --> [you, have, been].
+present_continuous --> [he, has, been].
+present_continuous --> [she, has, been].
+present_continuous --> [it, has, been].
+present_continuous --> [we, have, been].
+present_continuous --> [they, have, been].
+
+past_perfect --> [i, had].
+past_perfect --> [you, had].
+past_perfect --> [he, had].
+past_perfect --> [she, had].
+past_perfect --> [it, had].
+past_perfect --> [we, had].
+past_perfect --> [they, had].
+
+future_perfect --> [i, will, have].
+future_perfect --> [you, will, have].
+future_perfect --> [he, will, have].
+future_perfect --> [she, will, have].
+future_perfect --> [it, will, have].
+future_perfect --> [we, will, have].
+future_perfect --> [they, wil, have].
+
+future_perfect_progressive --> [i, will, have, been].
+future_perfect_progressive --> [you, will, have, been].
+future_perfect_progressive --> [he, will, have, been].
+future_perfect_progressive --> [she, will, have, been].
+future_perfect_progressive --> [it, will, have, been].
+future_perfect_progressive --> [we, will, have, been].
+future_perfect_progressive --> [they, wil, have, been].
+
+comparative --> [better, than].
+comparative --> [worse, than].
+comparative --> [prettier, than].
+comparative --> [stronger, than].
+
+superlative --> [worst].
+superlative --> [prettiest].
+superlative --> [strongest].
+superlative --> [coolest].
+superlative --> [bestest].
+
+superlative_adjective --> [most].
+
+object_pronoun_phrase --> verb_to_be, object_pronoun.
+object_pronoun_phrase --> distance, object_pronoun.
+
+comparative_phrase --> verb_to_be, comparative, object_pronoun. 
+
+superlative_phrase --> verb_to_be, determiner, superlative.
+superlative_phrase --> verb_to_be, determiner, superlative_adjective, adjective.
+
+gerund_phrase --> verb_to_be, gerund.
+
+adjective_phrase --> verb_to_be, adverb, adjective.
+adjective_phrase --> distance, adverb, adjective.
+
+modal_question --> modal, pronoun, verb.
+
+infinite_verb --> [to], verb.
+
+question_phrase --> modal_question, verb.
+question_phrase --> verb_to_be_question, gerund.
+question_phrase --> question, verb.
+
+reason_phrase --> pronoun, verb, infinite_verb.
+
+simple_past_phrase --> simple_past, gerund.
+simple_past_phrase --> simple_past, adjective.
+simple_past_phrase --> simple_past_negative, gerund.
+simple_past_phrase --> simple_past_negative, adjective.
+simple_past_phrase --> simple_past_question, gerund.
+simple_past_phrase --> simple_past_question, adjective.
+
+interrogative_pronoun_phrase --> interrogative_pronoun, verb_to_be_question.
+
+present_phrase --> pronoun, verb.
+present_phrase --> negative_verb_to_be, gerund.
+present_phrase --> negative_verb_to_be, merge_verb.
+present_phrase --> negative_verb_to_be, pronoun.
+
+past_phrase --> pronoun, past_verb.
+
+conditional_phrase --> pronoun, verb, conditional, pronoun, verb.
+
+logical_conditional_phrase --> pronoun, verb, logical_conditional, pronoun, verb.
+
+present_perfect_phrase --> present_perfect, participle.
+present_perfect_phrase --> present_perfect, participle.
+
+past_perfect_phrase --> past_perfect, participle.
+
+present_continuous_phrase --> present_continuous, gerund.
+
+absolute_possessive_phrase --> verb_to_be, absolute_possessive.
+absolute_possessive_phrase --> distance, absolute_possessive.
+
+possessive_phrase --> distance, possessive, singular_noun.
+possessive_phrase --> verb_to_be, possessive, singular_noun.
+
+tense --> verb_to_be, preposition, determiner, singular_noun.
+
+future_perfect_phrase --> future_perfect, participle.
+future_perfect_progressive_phrase --> future_perfect_progressive, gerund.
